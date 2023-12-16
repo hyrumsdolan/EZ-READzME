46ad2dc815e1a0a6110a6a169600b11a17aeb077
hyrumsdolan <hyrumsdolan@gmail.com>
2023-12-16 08:31:55 -0700

    generate markdown

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,20 +1,55 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title}
 
-`;
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+${renderLicenseBadge(data.license)}
+${renderLicenseSection(data.license)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+For any questions, please contact me using the information below:
+- GitHub: [${data.github}](https://github.com/${data.github})
+- Email: ${data.email}
+  `;
 }
+
+
+
+function renderLicenseBadge(license) {
+  if (!license) return "";
+  return `![License](https://img.shields.io/badge/license-${encodeURIComponent(license)}-blue.svg)`;
+}
+function renderLicenseLink(license) {
+  if (!license) return "";
+  return `https://opensource.org/licenses/${license}`;
+}
+function renderLicenseSection(license) {
+  if (!license) return "";
+  return `This project is licensed under the ${license} license. For more information, see the [license link](${renderLicenseLink(license)}).`;
+}
+
 
 module.exports = generateMarkdown;
